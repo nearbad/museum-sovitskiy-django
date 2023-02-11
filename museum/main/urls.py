b7cf650d7438711ec728868497479ef1936acc2a
nearbad <102875490+nearbad@users.noreply.github.com>
2023-02-11 12:53:01 +0300

    added another templates of weg pages not related with models

--- museum/main/urls.py
+++ museum/main/urls.py
@@ -5,4 +5,11 @@ app_name = 'main'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('museum_of_Nukus/', nks_museum, name='nks_museum'),
+    path('collections/', collections, name='collections'),
+    path('sources/', sources, name='sources'),
+    path('catalogue/', catalogue, name='catalogue'),
+    path('credits/', credits, name='credits'),
+    path('copyright/', copyright, name='copyright'),
+    path('contact/', contact, name='contact'),
 ]
--- museum/main/views.py
+++ museum/main/views.py
@@ -3,3 +3,31 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def nks_museum(request):
+    return render(request, 'main/nks_museum.html')
+
+
+def collections(request):
+    return render(request, 'main/collections.html')
+
+
+def sources(request):
+    return render(request, 'main/sources.html')
+
+
+def catalogue(request):
+    return render(request, 'main/catalogue.html')
+
+
+def credits(request):
+    return render(request, 'main/credits.html')
+
+
+def copyright(request):
+    return render(request, 'main/copyright.html')
+
+
+def contact(request):
+    return render(request, 'main/contact.html')
